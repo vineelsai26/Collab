9cc55789d05444ee02de38020c574bae1370bd02
Vineel Sai <mail@vineelsai.com>
2022-04-27 20:12:43 +0530

    delete doc

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,7 +1,9 @@
-import { Paper } from "@mui/material"
+import { IconButton, Paper } from "@mui/material"
 import { Box } from "@mui/system"
 import Navbar from "../Navbar/Navbar"
 import { useState, useEffect } from 'react'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const SERVER = process.env.REACT_APP_BACKEND_URL
 
@@ -10,6 +12,8 @@ function App() {
 	if (!!!user) {
 		window.location.href = '/login'
 	}
+
+	const [deleteVisible, setDeleteVisible] = useState({})
 
 	const [docs, setDocs] = useState([])
 
@@ -33,6 +37,40 @@ function App() {
 		})
 	}, [])
 
+	const handleMouseOver = (id) => {
+		setDeleteVisible({ ...deleteVisible, [id]: true })
+	}
+
+	const handleMouseLeave = (id) => {
+		setDeleteVisible({ ...deleteVisible, [id]: false })
+	}
+
+	const handleDocDelete = (e, id) => {
+		e.stopPropagation()
+		fetch(SERVER + '/deleteDoc', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: id })
+		}).then(async (res) => {
+			if (res.status === 200) {
+				const data = await res.json()
+				if (!!data) {
+					setDocs(docs.filter(doc => doc.id !== id))
+				}
+			}
+		}).catch(err => {
+			console.log(err)
+		})
+	}
+
+	const handleDocEdit = (e, id) => {
+		e.stopPropagation()
+		window.location.href = `/docs/${id}`
+	}
+
 	return (
 		<div style={{ backgroundColor: '#F5F5F5' }}>
 			<Navbar user={user} />
@@ -48,9 +86,17 @@ function App() {
 					return (
 						<Paper key={doc.id} sx={{ width: '150px', height: '200px', margin: '20px', cursor: 'pointer', textAlign: 'inherit', float: 'left', position: 'relative' }} onClick={() => window.location.href = `/docs/${doc.id}`}>
 							<img style={{ position: 'absolute', width: '100%' }} src="/icon.png" ></img>
-							<p style={{ textAlign: 'center', bottom: 0, position: 'absolute', width: '100%' }}>
-								{doc.title}
-							</p>
+							<Box onMouseOver={() => handleMouseOver(doc.id)} onMouseLeave={() => handleMouseLeave(doc.id)}>
+								<p style={{ textAlign: 'center', bottom: 0, position: 'absolute', width: '100%' }}>
+									{doc.title}
+								</p>
+								<IconButton style={{ zIndex: 1000, position: 'absolute', bottom: 0, left: 0, visibility: deleteVisible[doc.id] ? 'visible' : 'hidden' }} onClick={(e) => handleDocEdit(e, doc.id)}>
+									<EditIcon />
+								</IconButton>
+								<IconButton style={{ zIndex: 1000, position: 'absolute', bottom: 0, right: 0, visibility: deleteVisible[doc.id] ? 'visible' : 'hidden' }} onClick={(e) => handleDocDelete(e, doc.id)}>
+									<DeleteIcon />
+								</IconButton>
+							</Box>
 						</Paper>
 					)
 				})}
