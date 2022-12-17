1b52c658457dda22a355869d10b7aed277202cf8
Vineel Sai <mail@vineelsai.com>
2022-12-17 09:26:39 +0000

    change url for images

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -80,12 +80,12 @@ function App() {
 						width: '100%', position: 'relative',
 						top: '50%',
 						transform: 'translateY(-50%)',
-					}} src={'/public/new.webp'} />
+					}} src={'/new.webp'} />
 				</Paper>
 				{docs.map((doc) => {
 					return (
 						<Paper key={doc.id} sx={{ width: '150px', height: '200px', margin: '20px', cursor: 'pointer', textAlign: 'inherit', float: 'left', position: 'relative' }} onClick={() => window.location.href = `/docs/${doc.id}`}>
-							<img style={{ position: 'absolute', width: '100%' }} src="/public/icon.png" ></img>
+							<img style={{ position: 'absolute', width: '100%' }} src="/icon.png" ></img>
 							<Box onMouseOver={() => handleMouseOver(doc.id)} onMouseLeave={() => handleMouseLeave(doc.id)}>
 								<p style={{ textAlign: 'center', bottom: 0, position: 'absolute', width: '100%' }}>
 									{doc.title}
